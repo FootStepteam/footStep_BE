db378ecf9997f1e2dfa86b871ff97a429d029952
TaehoLEEKR <679748@naver.com>
2023-06-29 15:02:35 +0900

    fix : 004 - ChattingTest
    body : merge error

--- src/main/java/com/example/footstep/config/WebSocketConfig.java
+++ src/main/java/com/example/footstep/config/WebSocketConfig.java
@@ -2,39 +2,22 @@ package com.example.footstep.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.server.ServerHttpRequest;
-import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.*;
-import org.springframework.web.socket.server.HandshakeFailureException;
-import org.springframework.web.socket.server.HandshakeHandler;
-
-import java.net.URI;
-import java.util.Map;
 
 
-@RequiredArgsConstructor
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/sub");
-        config.setApplicationDestinationPrefixes("/pub");
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
     }
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-stomp")
-                .setAllowedOrigins("*")
-                .setHandshakeHandler(new HandshakeHandler() {
-                    @Override
-                    public boolean doHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws HandshakeFailureException {
-                        String wsUri = "ws://" + request.getURI().getHost() + ":" + request.getURI().getPort() + request.getURI().getPath();
-                        attributes.put("websocketUri", URI.create(wsUri));
-                        return true;
-                    }
-                })
+        registry.addEndpoint("/javatechie")
+                .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 }
--- src/main/java/com/example/footstep/controller/ChatController.java
+++ src/main/java/com/example/footstep/controller/ChatController.java
@@ -1,25 +1,24 @@
 package com.example.footstep.controller;
 
 import com.example.footstep.domain.dto.chat.ChatMessageDto;
-import com.example.footstep.domain.dto.chat.ChatRoomDto;
-import com.example.footstep.service.ChatService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
 
-import java.util.List;
-
-@RequiredArgsConstructor
-@RestController
-@RequestMapping("/chat")
+@Controller
 public class ChatController {
-    private final SimpMessageSendingOperations messagingTemplate;
+    @MessageMapping("/chat.register")
+    @SendTo("/topic/public")
+    public ChatMessageDto register(@Payload ChatMessageDto chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
 
-    @MessageMapping("/chat/message")
-    public void message(ChatMessageDto message) {
-        if (ChatMessageDto.MessageType.JOIN.equals(message.getType()))
-            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
-        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+    @MessageMapping("/chat.send")
+    @SendTo("/topic/public")
+    public ChatMessageDto sendMessage(@Payload ChatMessageDto chatMessage) {
+        return chatMessage;
     }
 }
