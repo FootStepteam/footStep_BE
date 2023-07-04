064d98b7f5c7ed73dc5b36cac42d73cd74b677fb
impath96 <impath96@gmail.com>
2023-07-04 14:18:32 +0900

    refactor : 회원 정보 데이터 추가

--- src/main/java/com/example/footstep/model/dto/member/MemberProfileDto.java
+++ src/main/java/com/example/footstep/model/dto/member/MemberProfileDto.java
@@ -3,25 +3,36 @@ package com.example.footstep.model.dto.member;
 import com.example.footstep.model.entity.Member;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class MemberProfileDto {
 
     private Long memberId;
+    private String loginEmail;
     private String nickname;
-    private String imgUrl;
+    private String password;
+    private String gender;
+    private String img;
+    private String description;
 
 
     public static MemberProfileDto from(Member member) {
-
         return MemberProfileDto.builder()
             .memberId(member.getMemberId())
             .nickname(member.getNickname())
-            .imgUrl(member.getImg())
+            .gender(member.getGender())
+            .img(member.getImg())
+            .password(member.getPassword())
+            .loginEmail(member.getLoginEmail())
+            .description(member.getDescription())
             .build();
-
     }
+
 }
